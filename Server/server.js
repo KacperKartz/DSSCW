8c180be933e7ab5149e5cd9fef05081be3e3fa53
Kartzfx <kacperkartz@gmail.com>
2025-02-19 17:12:48 +0000

    Set up server and client

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
