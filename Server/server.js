f547ac29bc1500b217504cb5c96874d42373ea32
Kartzfx <kacperkartz@gmail.com>
2025-03-02 15:50:13 +0000

    Added .env to gitignore Added /blog in backend

--- Server/server.js
+++ Server/server.js
@@ -8,8 +8,12 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "../Client/dist")));
 
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
+});
+
+app.get("/blog", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/dist", "blog.html"));
 });
 
 
